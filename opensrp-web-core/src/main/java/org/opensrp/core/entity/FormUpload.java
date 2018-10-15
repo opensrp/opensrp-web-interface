9e2b1676e99095f935b8682f806e6abe5359ae34
Prince1126090 <prince1126090@gmail.com>
2018-10-15 18:08:05 +0600

    add proper annotation for user

--- opensrp-web-core/src/main/java/org/opensrp/core/entity/FormUpload.java
+++ opensrp-web-core/src/main/java/org/opensrp/core/entity/FormUpload.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -29,7 +31,8 @@ public class FormUpload {
 	@Column(name = "name")
 	private String name;
 	
-	@Column(name = "user")
+	@ManyToOne()
+	@JoinColumn(name = "user", referencedColumnName = "id")
 	private User user;
 	
 	@Column(name = "time_stamp")
