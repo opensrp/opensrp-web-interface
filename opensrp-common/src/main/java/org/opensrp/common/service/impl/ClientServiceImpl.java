03390d7d5bf4d30ff37cbc86220201275ea8b2d8
Prince1126090 <prince1126090@gmail.com>
2018-10-15 15:09:43 +0600

    remove sysout

--- opensrp-common/src/main/java/org/opensrp/common/service/impl/ClientServiceImpl.java
+++ opensrp-common/src/main/java/org/opensrp/common/service/impl/ClientServiceImpl.java
@@ -12,23 +12,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.opensrp.common.entity.ClientEntity;
+import org.opensrp.common.interfaces.DatabaseRepository;
 import org.opensrp.common.interfaces.DatabaseService;
-import org.opensrp.common.repository.impl.DatabaseRepositoryImpl;
 import org.opensrp.common.util.EntityProperties;
 import org.opensrp.common.visualization.HighChart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ClientServiceImpl extends EntityProperties implements DatabaseService {
+public class ClientServiceImpl extends EntityProperties implements
+		DatabaseService {
 
-	private static final Logger logger = Logger.getLogger(ClientServiceImpl.class);
+	private static final Logger logger = Logger
+			.getLogger(ClientServiceImpl.class);
 
 	@Autowired
-	private DatabaseServiceImpl databaseServiceImpl;
-
-	@Autowired
-	private DatabaseRepositoryImpl databaseRepositoryImpl;
+	private DatabaseRepository repository;
 
 	@Autowired
 	private OpenSRPClientServiceImpl openSRPClientServiceImpl;
@@ -37,11 +36,10 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 
 	}
 
-
 	@Transactional
 	@Override
 	public <T> long save(T t) throws Exception {
-		return databaseRepositoryImpl.save(t);
+		return repository.save(t);
 	}
 
 	@Transactional
@@ -53,57 +51,55 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 	@Transactional
 	@Override
 	public <T> T findById(int id, String fieldName, Class<?> className) {
-		return databaseRepositoryImpl.findById(id, fieldName, className);
+		return repository.findById(id, fieldName, className);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Transactional
 	@Override
 	public <T> List<T> findAll(String tableClass) {
-		return (List<T>) databaseRepositoryImpl.findAll(tableClass);
+		return (List<T>) repository.findAll(tableClass);
 	}
 
 	@Transactional
 	@Override
 	public <T> T findByKey(String value, String fieldName, Class<?> className) {
-		return databaseRepositoryImpl.findByKey(value, fieldName, className);
+		return repository.findByKey(value, fieldName, className);
 	}
 
 	@Transactional
 	@Override
 	public <T> long update(T t) throws Exception {
-		return databaseRepositoryImpl.update(t);
+		return repository.update(t);
 	}
 
 	@Transactional
-	public void getChildWeightList(HttpSession session,String id) throws JSONException{
-		System.out.println("Child id :" + id);
+	public void getChildWeightList(HttpSession session, String id)
+			throws JSONException {
 		session.setAttribute("childId", id);
 
 		List<Object[]> weightList;
 
 		String weightQuery = "SELECT * FROM core.child_growth "
-				+" WHERE base_entity_id = '"
-				+id
-				+"' " 
-				+" ORDER BY event_date ASC";
-		weightList = databaseServiceImpl.executeSelectQuery(weightQuery);
+				+ " WHERE base_entity_id = '" + id + "' "
+				+ " ORDER BY event_date ASC";
+		weightList = repository.executeSelectQuery(weightQuery);
 
-		//for line chart
+		// for line chart
 		List<Object[]> weightForChart = new ArrayList<Object[]>();
-		List<Object[]> growthForChart= new ArrayList<Object[]>();
+		List<Object[]> growthForChart = new ArrayList<Object[]>();
 
 		Iterator weightListIterator = weightList.iterator();
-		int i=0;
+		int i = 0;
 		while (weightListIterator.hasNext()) {
 			Object[] weightObject = (Object[]) weightListIterator.next();
 			Object[] rowWeightData = new Object[2];
 			Object[] rowGrowthData = new Object[2];
-			Double weight = Double.parseDouble(String.valueOf(weightObject[13]));
-			Double growth = Double.parseDouble(String.valueOf(weightObject[17]));
+			Double weight = Double
+					.parseDouble(String.valueOf(weightObject[13]));
+			Double growth = Double
+					.parseDouble(String.valueOf(weightObject[17]));
 			growth /= 1000.00;
-			/*System.out.println(weight);
-			System.out.println(growth);*/
 			rowWeightData[0] = i;
 			rowWeightData[1] = weight;
 			weightForChart.add(rowWeightData);
@@ -112,8 +108,10 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 			rowGrowthData[1] = growth;
 			growthForChart.add(rowGrowthData);
 		}
-		JSONArray lineChartWeightData = HighChart.getLineChartData(weightForChart, "Weight");
-		JSONArray lineChartGrowthData = HighChart.getLineChartData(growthForChart, "Growth");
+		JSONArray lineChartWeightData = HighChart.getLineChartData(
+				weightForChart, "Weight");
+		JSONArray lineChartGrowthData = HighChart.getLineChartData(
+				growthForChart, "Growth");
 
 		session.setAttribute("weightList", weightList);
 		session.setAttribute("lineChartWeightData", lineChartWeightData);
@@ -121,13 +119,12 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 	}
 
 	@Transactional
-	public void getMotherDetails(HttpSession session,String id){
-
-		System.out.println("Mother id :" + id);
+	public void getMotherDetails(HttpSession session, String id) {
 		session.setAttribute("motherId", id);
 
 		List<Object> data;
-		data = databaseServiceImpl.getDataFromViewByBEId("viewJsonDataConversionOfEvent","mother",id);
+		data = repository.getDataFromViewByBEId(
+				"viewJsonDataConversionOfEvent", "mother", id);
 		session.setAttribute("eventList", data);
 
 		List<Object> NWMRList = new ArrayList<Object>();
@@ -138,15 +135,13 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 			Object[] eventObject = (Object[]) dataListIterator.next();
 
 			String eventType = String.valueOf(eventObject[8]);
-			//System.out.println(eventType);
 
-			if(eventType.equals("New Woman Member Registration")){
-				//System.out.println(eventObject);
+			if (eventType.equals("New Woman Member Registration")) {
 				NWMRList.add(eventObject);
-				//System.out.println(NWMRList);
-			}else if(eventType.equals("Pregnant Woman Counselling") || eventType.equals("Lactating Woman Counselling")){
+			} else if (eventType.equals("Pregnant Woman Counselling")
+					|| eventType.equals("Lactating Woman Counselling")) {
 				counsellingList.add(eventObject);
-			}else if(eventType.equals("Woman Member Follow Up")){
+			} else if (eventType.equals("Woman Member Follow Up")) {
 				followUpList.add(eventObject);
 			}
 		}
@@ -158,8 +153,11 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 
 	public void updateClientData(ClientEntity clientEntity, String baseEntityId)
 			throws JSONException {
-		List<Object> clientData = databaseServiceImpl.executeSelectQuery("select id, cast(json as character varying) from core.client " 
-				+ " where json->>'baseEntityId' = '" + baseEntityId + "'");
+		List<Object> clientData = repository
+				.executeSelectQuery("select id, cast(json as character varying) from core.client "
+						+ " where json->>'baseEntityId' = '"
+						+ baseEntityId
+						+ "'");
 
 		JSONObject jsonObject = new JSONObject();
 
@@ -172,20 +170,29 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 
 				JSONObject jsonFromClient = new JSONObject(jsonData);
 
-				if(clientEntity.getFirstName() != null && !clientEntity.getFirstName().isEmpty()) {
-					jsonFromClient.put("firstName", clientEntity.getFirstName());
+				if (clientEntity.getFirstName() != null
+						&& !clientEntity.getFirstName().isEmpty()) {
+					jsonFromClient
+							.put("firstName", clientEntity.getFirstName());
 				}
-				if (clientEntity.getLastName() != null && !clientEntity.getLastName().isEmpty()) {
+				if (clientEntity.getLastName() != null
+						&& !clientEntity.getLastName().isEmpty()) {
 					jsonFromClient.put("lastName", clientEntity.getLastName());
 				}
-				if (clientEntity.getPhoneNumber() != null && !clientEntity.getPhoneNumber().isEmpty()) {
-					jsonFromClient.getJSONObject("attributes").put("phoneNumber", clientEntity.getPhoneNumber());
+				if (clientEntity.getPhoneNumber() != null
+						&& !clientEntity.getPhoneNumber().isEmpty()) {
+					jsonFromClient.getJSONObject("attributes").put(
+							"phoneNumber", clientEntity.getPhoneNumber());
 				}
-				if(clientEntity.getNid() != null && !clientEntity.getNid().isEmpty()) {
-					jsonFromClient.getJSONObject("attributes").put("nationalId", clientEntity.getNid());
+				if (clientEntity.getNid() != null
+						&& !clientEntity.getNid().isEmpty()) {
+					jsonFromClient.getJSONObject("attributes").put(
+							"nationalId", clientEntity.getNid());
 				}
-				if (clientEntity.getSpouseName() != null && !clientEntity.getSpouseName().isEmpty()) {
-					jsonFromClient.getJSONObject("attributes").put("spouseName", clientEntity.getSpouseName());
+				if (clientEntity.getSpouseName() != null
+						&& !clientEntity.getSpouseName().isEmpty()) {
+					jsonFromClient.getJSONObject("attributes").put(
+							"spouseName", clientEntity.getSpouseName());
 				}
 
 				JSONArray jsonArrayForClient = new JSONArray();
@@ -198,13 +205,10 @@ public class ClientServiceImpl extends EntityProperties implements DatabaseServi
 	}
 
 	public String getHouseholdEntityNamePrefix() {
-		System.out.println("entity properties: " + PROJECT_ENTITY);
 		return PROJECT_ENTITY;
 	}
 
 	public String getWomanEntityName() {
-		System.out.println("woman properties: " + WOMAN_ENTITY);
 		return WOMAN_ENTITY;
 	}
 }
-
