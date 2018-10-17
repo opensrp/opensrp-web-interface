fd69a23b9edb806898017e6a62fe480886c62532
Prince1126090 <prince1126090@gmail.com>
2018-10-17 16:56:55 +0600

    remove - implements DatabaseService

--- opensrp-web-core/src/main/java/org/opensrp/core/service/FormService.java
+++ opensrp-web-core/src/main/java/org/opensrp/core/service/FormService.java
@@ -6,13 +6,11 @@ import javax.transaction.Transactional;
 
 import org.apache.log4j.Logger;
 import org.opensrp.common.interfaces.DatabaseRepository;
-import org.opensrp.common.interfaces.DatabaseService;
-import org.opensrp.common.util.EntityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FormService extends EntityProperties implements DatabaseService {
+public class FormService {
 	
 	private static final Logger logger = Logger.getLogger(FormService.class);
 	
@@ -24,38 +22,32 @@ public class FormService extends EntityProperties implements DatabaseService {
 	}
 	
 	@Transactional
-	@Override
 	public <T> long save(T t) throws Exception {
 		return repository.save(t);
 	}
 	
 	@Transactional
-	@Override
 	public <T> int delete(T t) {
 		return 0;
 	}
 	
 	@Transactional
-	@Override
 	public <T> T findById(int id, String fieldName, Class<?> className) {
 		return repository.findById(id, fieldName, className);
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Transactional
-	@Override
 	public <T> List<T> findAll(String tableClass) {
 		return (List<T>) repository.findAll(tableClass);
 	}
 	
 	@Transactional
-	@Override
 	public <T> T findByKey(String value, String fieldName, Class<?> className) {
 		return repository.findByKey(value, fieldName, className);
 	}
 	
 	@Transactional
-	@Override
 	public <T> long update(T t) throws Exception {
 		return repository.update(t);
 	}
