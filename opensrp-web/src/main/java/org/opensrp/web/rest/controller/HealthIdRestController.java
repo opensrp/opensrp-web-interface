8c58ab4fdccf17f55b7535b4c7bc187d59ae8eb7
proshanto <proshanto123@gmail.com>
2019-02-14 17:49:55 +0600

    change in health id api

--- opensrp-web/src/main/java/org/opensrp/web/rest/controller/HealthIdRestController.java
+++ opensrp-web/src/main/java/org/opensrp/web/rest/controller/HealthIdRestController.java
@@ -3,6 +3,7 @@ package org.opensrp.web.rest.controller;
 import static org.springframework.http.HttpStatus.OK;
 
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 import org.opensrp.core.service.HealthIdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,13 +35,14 @@ public class HealthIdRestController {
 	
 	@RequestMapping(value = "/reserved/single", method = RequestMethod.GET)
 	public ResponseEntity<String> getSingleResearvedHealthId() throws Exception {
-		
+		JSONObject noContent = new JSONObject();
+		noContent.put("identifiers", "");
 		try {
 			return new ResponseEntity<>(healthIdService.getSingleHealthIdAndUpdateRecord().toString(), OK);
 		}
 		catch (Exception e) {
 			logger.error("health id error:" + e);
 		}
-		return new ResponseEntity<>("No Data Found", HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(noContent.toString(), HttpStatus.NO_CONTENT);
 	}
 }
