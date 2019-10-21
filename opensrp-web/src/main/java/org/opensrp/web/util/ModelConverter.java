da354d1915325f0412f44577a8aee81402bcfca3
Jysan Aziz <jeexan2@gmail.com>
2019-10-21 12:26:51 +0600

    client report service update

--- opensrp-web/src/main/java/org/opensrp/web/util/ModelConverter.java
+++ opensrp-web/src/main/java/org/opensrp/web/util/ModelConverter.java
@@ -8,7 +8,7 @@ import java.util.List;
 public  class ModelConverter {
     public static List<Object[]> modelConverterForClientData(String formName, List<Object[]> dataList){
         List<Object[]> ret = new ArrayList<>();
-        if(formName.equals("Member Registration")) {
+        if(formName.equals("Member Registration") || formName.equals("Family Member Registration")) {
             for(Object[] row: dataList){
                 Object[] newRow = new Object[13] ;
                 newRow[0] = row[0];
@@ -19,7 +19,7 @@ public  class ModelConverter {
                 ret.add(newRow);
             }
         }
-        else if(formName.equals("Household Registration")){
+        else if(formName.equals("Household Registration") || formName.equals("Family Registration")){
             for(Object[] row: dataList){
                 Object[] newRow = new Object[11];
                 newRow[0] = row[0];
@@ -38,14 +38,14 @@ public  class ModelConverter {
     public static List<String> headerListForClientData(String formName){
         List<String> ret = new ArrayList<>();
         String [] ar = new String[]{};
-        if(formName.equals("Member Registration")) {
+        if(formName.equals("Member Registration") || formName.equals("Family Member Registration")) {
              String[] arTemp = {"Id", "Relation with HOH","Mother Name", "Mobile Number",
                             "Id Type","NID Number","Birth Id Number","DOB_Known","Date of Birth","Age", "Gender"
                             ,"Marital Status", "Blood Group"};
 
              ar = arTemp;
         }
-        else if(formName.equals("Household Registration")){
+        else if(formName.equals("Household Registration")|| formName.equals("Family Registration")){
             String[] arTemp = {"Id","SS Name","Village Name","Cluster","Household Type",
                     "HH Number","Household Number",
                                 "Household Name","Household Phone Number", "Number of Household Member"
