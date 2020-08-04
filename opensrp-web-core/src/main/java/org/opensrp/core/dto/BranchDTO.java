99efd68d7c3eb584b91ae921ba87c5fb747813b7
prodhan29 <02nahid02@gmail.com>
2020-08-04 19:41:55 +0600

    add new fields to new-branch-form. functionality working fine for create action. need to work on edit-action

--- opensrp-web-core/src/main/java/org/opensrp/core/dto/BranchDTO.java
+++ opensrp-web-core/src/main/java/org/opensrp/core/dto/BranchDTO.java
@@ -8,6 +8,20 @@ public class BranchDTO {
 
     private String code;
 
+    private Integer division;
+
+    private Integer district;
+
+    private Integer upazila;
+
+    private Integer skPosition;
+
+    private Integer ssPosition;
+
+    private Integer paPosition;
+
+    private Integer pkPosition;
+
     public int getId() {
         return id;
     }
@@ -32,12 +46,75 @@ public class BranchDTO {
         this.code = code;
     }
 
+    public Integer getDivision() {
+        return division;
+    }
+
+    public void setDivision(Integer division) {
+        this.division = division;
+    }
+
+    public Integer getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(Integer district) {
+        this.district = district;
+    }
+
+    public Integer getUpazila() {
+        return upazila;
+    }
+
+    public void setUpazila(Integer upazila) {
+        this.upazila = upazila;
+    }
+
+    public Integer getSkPosition() {
+        return skPosition;
+    }
+
+    public void setSkPosition(Integer skPosition) {
+        this.skPosition = skPosition;
+    }
+
+    public Integer getSsPosition() {
+        return ssPosition;
+    }
+
+    public void setSsPosition(Integer ssPosition) {
+        this.ssPosition = ssPosition;
+    }
+
+    public Integer getPaPosition() {
+        return paPosition;
+    }
+
+    public void setPaPosition(Integer paPosition) {
+        this.paPosition = paPosition;
+    }
+
+    public Integer getPkPosition() {
+        return pkPosition;
+    }
+
+    public void setPkPosition(Integer pkPosition) {
+        this.pkPosition = pkPosition;
+    }
+
     @Override
     public String toString() {
         return "BranchDTO{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", code='" + code + '\'' +
+                ", division=" + division +
+                ", district=" + district +
+                ", upazila=" + upazila +
+                ", skPosition=" + skPosition +
+                ", ssPosition=" + ssPosition +
+                ", paPosition=" + paPosition +
+                ", pkPosition=" + pkPosition +
                 '}';
     }
 }
