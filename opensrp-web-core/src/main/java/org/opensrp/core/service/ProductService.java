78e6090954b09faa2f461fa81bd18f3689a7907c
proshanto <proshanto123@gmail.com>
2020-08-12 14:49:41 +0600

    change one method

--- opensrp-web-core/src/main/java/org/opensrp/core/service/ProductService.java
+++ opensrp-web-core/src/main/java/org/opensrp/core/service/ProductService.java
@@ -4,10 +4,8 @@
 
 package org.opensrp.core.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -65,19 +63,6 @@ public class ProductService extends CommonService {
 		}
 		
 		return result;
-	}
-	
-	public List<ProductDTO> productListFortStockIn(Integer branchId, Integer roleId) {
-		List<ProductDTO> products = productListByBranchWithCurrentStock(branchId, roleId);
-		
-		List<Long> pids = new ArrayList<>();
-		
-		for (ProductDTO productDTO : products) {
-			pids.add(productDTO.getId());
-		}
-		products.addAll(productListWithoutBranch(StringUtils.join(pids.toArray(), ", "), roleId));
-		
-		return products;
 	}
 	
 }
--- opensrp-web-core/src/main/java/org/opensrp/core/service/RequisitionService.java
+++ opensrp-web-core/src/main/java/org/opensrp/core/service/RequisitionService.java
@@ -4,7 +4,13 @@
 
 package org.opensrp.core.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.opensrp.core.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +18,23 @@ public class RequisitionService extends CommonService {
 	
 	private static final Logger logger = Logger.getLogger(RequisitionService.class);
 	
+	@Autowired
+	private ProductService productService;
+	
 	public RequisitionService() {
 		
 	}
 	
+	public List<ProductDTO> productListFortRequisition(Integer branchId, Integer roleId) {
+		List<ProductDTO> products = productService.productListByBranchWithCurrentStock(branchId, roleId);
+		
+		List<Long> pids = new ArrayList<>();
+		
+		for (ProductDTO productDTO : products) {
+			pids.add(productDTO.getId());
+		}
+		products.addAll(productService.productListWithoutBranch(StringUtils.join(pids.toArray(), ", "), roleId));
+		
+		return products;
+	}
 }
