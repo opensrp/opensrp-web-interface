27740507c521a2e9fb7d53262b0e2b0f5e646981
proshanto <proshanto123@gmail.com>
2020-08-13 12:05:40 +0600

    chnage on stock API

--- opensrp-web-core/src/main/java/org/opensrp/core/service/StockService.java
+++ opensrp-web-core/src/main/java/org/opensrp/core/service/StockService.java
@@ -4,7 +4,25 @@
 
 package org.opensrp.core.service;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.opensrp.common.util.ReferenceType;
+import org.opensrp.common.util.Status;
+import org.opensrp.core.dto.StockDTO;
+import org.opensrp.core.dto.StockDetailsDTO;
+import org.opensrp.core.entity.Stock;
+import org.opensrp.core.entity.StockDetails;
+import org.opensrp.core.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,4 +34,74 @@ public class StockService extends CommonService {
 		
 	}
 	
+	@Transactional
+	public <T> Integer saveAll(StockDTO dto) throws Exception {
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		Integer returnValue = null;
+		
+		try {
+			tx = session.beginTransaction();
+			
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			User user = (User) auth.getPrincipal();
+			Set<StockDetailsDTO> stockDetailsDTOs = dto.getStockDetailsDTOs();
+			Set<Integer> sellTos = dto.getSellTo();
+			for (Integer sellTo : sellTos) {
+				Stock stock = findById(dto.getId(), "id", Stock.class);
+				if (stock == null) {
+					stock = new Stock();
+				}
+				if (stock.getId() != null) {
+					stock.setStockDetails(null);
+					stock.setUpdatedBy(user);
+					boolean isDelete = deleteAllByPrimaryKey(stock.getId(), "_stock_details", "stock_id");
+					if (!isDelete) {
+						return null;
+					}
+				} else {
+					stock.setCreator(user);
+					stock.setUuid(UUID.randomUUID().toString());
+				}
+				Set<StockDetails> _stockDetails = new HashSet<>();
+				for (StockDetailsDTO stockDetailsDTO : stockDetailsDTOs) {
+					StockDetails stockDetails = new StockDetails();
+					stockDetails.setBranchId(stockDetailsDTO.getBranchId());
+					stockDetails.setCredit(stockDetailsDTO.getCredit());
+					stockDetails.setDebit(stockDetailsDTO.getDebit());
+					stockDetails.setExpireyDate(stockDetailsDTO.getExpireyDate());
+					stockDetails.setReceiveDate(stockDetailsDTO.getReceiveDate());
+					stockDetails.setProductId(stockDetailsDTO.getProductId());
+					stockDetails.setReferenceType(ReferenceType.valueOf(stockDetailsDTO.getReferenceType()).name());
+					stockDetails.setSellOrPassTo(sellTo);
+					stockDetails.setTimestamp(System.currentTimeMillis());
+					stockDetails.setStatus(Status.valueOf(stockDetailsDTO.getStatus()).name());
+					stockDetails.setInvoiceNumber(stockDetailsDTO.getInvoiceNumber());
+					stockDetails.setStock(stock);
+					stockDetails.setMonth(stockDetailsDTO.getMonth());
+					stockDetails.setYear(stockDetailsDTO.getYear());
+					stockDetails.setStartDate(stockDetailsDTO.getStartDate());
+					_stockDetails.add(stockDetails);
+					
+				}
+				stock.setStockDetails(_stockDetails);
+				session.saveOrUpdate(stock);
+				
+			}
+			
+			if (!tx.wasCommitted())
+				tx.commit();
+			
+			returnValue = 1;
+		}
+		catch (HibernateException e) {
+			tx.rollback();
+			logger.error(e);
+			throw new Exception(e.getMessage());
+		}
+		finally {
+			session.close();
+		}
+		return returnValue;
+	}
 }
--- opensrp-web/src/main/java/org/opensrp/web/rest/controller/StockRestController.java
+++ opensrp-web/src/main/java/org/opensrp/web/rest/controller/StockRestController.java
@@ -4,8 +4,6 @@ import static org.springframework.http.HttpStatus.OK;
 
 import org.json.JSONObject;
 import org.opensrp.core.dto.StockDTO;
-import org.opensrp.core.entity.Stock;
-import org.opensrp.core.mapper.StockMapper;
 import org.opensrp.core.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -24,33 +22,27 @@ public class StockRestController {
 	@Autowired
 	private StockService stockService;
 	
-	@Autowired
-	private StockMapper stockMapper;
-	
 	@RequestMapping(value = "/save-update", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> newPatient(@RequestBody StockDTO dto) throws Exception {
-		Stock stock = stockService.findById(dto.getId(), "id", Stock.class);
+		
 		JSONObject response = new JSONObject();
-		System.err.println(dto);
+		
 		try {
-			if (stock != null) {
-				stock = stockMapper.map(dto, stock);
+			
+			Integer isSave = stockService.saveAll(dto);
+			if (isSave != null) {
+				response.put("status", "SUCCESS");
+				response.put("msg", "you have created successfully");
 			} else {
-				stock = new Stock();
-				stock = stockMapper.map(dto, stock);
+				response.put("status", "FAILED");
+				response.put("msg", "Something went worng please contact with admin.");
 			}
-			System.out.println(stock.toString());
-			if (stock != null) {
-				
-				stockService.save(stock);
-			}
-			response.put("status", "SUCCESS");
-			response.put("msg", "you have created successfully");
+			
 			return new ResponseEntity<>(new Gson().toJson(response.toString()), OK);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
-			response.put("status", "FAILED");
+			
 			response.put("msg", e.getMessage());
 			return new ResponseEntity<>(new Gson().toJson(response.toString()), OK);
 		}
